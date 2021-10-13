a62b53a947a8122f7a50a544e54346b433518826
guillerman2001 <78086935+guillerman2001@users.noreply.github.com>
2021-10-13 17:02:30 +0200

    Segundo

--- reto2.1.ts
+++ reto2.1.ts
@@ -1,12 +1,12 @@
 export class Mobile{
-    public name: string;
-    public model: string;
-    public trademark: string;
-    public sdSize: number;
-    public color: string;
-    public is5G: boolean;
-    public cameraNumber: number;
-    public price: number;
+    private name: string;
+    private model: string;
+    private trademark: string;
+    private sdSize: number;
+    private color: string;
+    private is5G: boolean;
+    private cameraNumber: number;
+    private price: number;
     constructor(name: string, model: string, trademark: string, sdSize: number, color: string, is5G: boolean, cameraNumber: number, price: number){
         this.name = name;
         this.model = model;
@@ -17,4 +17,68 @@ export class Mobile{
         this.cameraNumber = cameraNumber;
         this.price = price;
     }
+    //GET
+    getName():string{
+        return this.name;
+    }
+    getModel():string{
+        return this.model;
+    }
+    getTrademark():string{
+        return this.trademark;
+    }
+    getSdSize():number{
+        return this.sdSize;
+    }
+    getColor():string{
+        return this.color;
+    }
+    getIs5G():boolean{
+        return this.is5G;
+    }
+    getCameraNumber():number{
+        return this.cameraNumber;
+    }
+    getPrice():number{
+        return this.price;
+    }
+    //SET
+    setName(name:string){
+        this.name = name
+    }
+    setModel(model:string){
+        this.model = model;
+    }
+    setTrademark(trademark:string){
+        this.trademark = trademark;
+    }
+    setSdSize(sdSize:number){
+        this.sdSize = sdSize;
+    }
+    setColor(color:string){
+        this.color = color;
+    }
+    setIs5G(is5G:boolean){
+        this.is5G = is5G;
+    }
+    setCameraNumber(cameraNumber:number){
+        this.cameraNumber = cameraNumber;
+    }
+    setPrice(price:number){
+        this.price = price;
+    }
+    printAll(){
+        console.log(`
+        The characteristics of the mobile ${this.name} are:
+            Name: ${this.name}
+            Model: ${this.model}
+            Trademark: ${this.trademark}
+            SD Size (GB): ${this.sdSize}
+            Color: ${this.color}
+            Is 5g?: ${this.is5G}
+            Number of Cameras: ${this.cameraNumber}
+        `);
+    }
 }
+
+
--- reto2.2.ts
+++ reto2.2.ts
@@ -4,13 +4,8 @@ let Nokia3210 = new Mobile('Nokia','Nokia3210','Nokia',6,'negro',false,1,30.99);
 let IPhone3G = new Mobile('IPhone','IPhone3G','aple',18,'blanco',false,3,150.99);
 let SamsungGalaxy10 = new Mobile('Samsung','Galaxy10','Samsung',32,'negro',true,4,70.99);
 
-console.log('Nokia',Nokia3210);
-console.log('Aple',IPhone3G);
-console.log('Samsung',SamsungGalaxy10);
+let arrayMob = [Nokia3210, IPhone3G, SamsungGalaxy10];
 
-Nokia3210.is5G = true;
-Nokia3210.cameraNumber = 4;
-
-console.log('Nokia',Nokia3210);
-console.log('Aple',IPhone3G);
-console.log('Samsung',SamsungGalaxy10);
+for(let array of arrayMob){
+    array.printAll();
+}
